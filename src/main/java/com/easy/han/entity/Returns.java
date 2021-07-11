4b46661e26e9be0b2d16988ba3bb5fe66241c3ad
zhaohan <1364793515@qq.com>
2021-07-11 14:22:59 +0800

    1.1

--- src/main/java/com/easy/han/entity/Returns.java
+++ src/main/java/com/easy/han/entity/Returns.java
@@ -1,4 +1,28 @@
 package com.easy.han.entity;
 
+import lombok.Data;
+
+/**
+ * @author zhaohan
+ */
+
+@Data
 public class Returns {
+
+    /**
+     * 回报标志
+     */
+    private String ack;
+
+    /**
+     * 工厂的总流水线数量
+     */
+    private int total;
+
+    /**
+     * 工厂的繁忙流水线数量
+     */
+    private int busy;
+
 }
+
--- src/main/java/com/easy/han/service/Implementer.java
+++ src/main/java/com/easy/han/service/Implementer.java
@@ -3,11 +3,10 @@ package com.easy.han.service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
 
+import java.util.ArrayList;
 import java.util.concurrent.Callable;
-import com.easy.han.entity.Order;
-import com.easy.han.entity.CommodityAttributes;
-import com.easy.han.entity.Bottle;
-import com.easy.han.entity.Water;
+
+import com.easy.han.entity.*;
 import com.easy.han.handler.MqttPublisher;
 import com.easy.han.handler.MqttSubscriber;
 
@@ -33,33 +32,73 @@ public class Implementer implements Callable {
     }
 
     private String json;
+
     @Override
     public String[] call() throws Exception {
 
-        if (){
+        ArrayList<String> tops = new ArrayList<>();
 
-            while (){
+        Order order1 = JSON.parseObject(json, Order.class);
+        String s_valss = order1.getS_vals();
+        CommodityAttributes attributess = JSON.parseObject(s_valss, CommodityAttributes.class);
+        int num1=attributess.getNum();
+
+        int surplus=num1;
+
+        while (surplus>0) {
+
+            String returns = ack();
+
+            Returns returns1 = JSON.parseObject(returns, Returns.class);
+            int total1 = returns1.getTotal();
+            int busy = returns1.getBusy();
+            int free = total1 - busy;
+
+            if (free < surplus) {
+
+                int o_id1 = order1.getO_id();
+                short capacity1 = attributess.getCapacity();
+                Bottle bottle = new Bottle(o_id1, capacity1, free);
+                String bottleTask1 = (String) JSON.toJSON(bottle);
+
+                String type1 = attributess.getType();
+                int total2 = free * capacity1;
+                Water water = new Water(o_id1, type1, total2);
+                String waterTask1 = (String) JSON.toJSON(water);
+
+                tops.add(bottleTask1);
+                tops.add(waterTask1);
+
+                surplus = surplus - free;
+
+            } else {
+
+                int o_id = order1.getO_id();
+
+                short capacity = attributess.getCapacity();
+                Bottle bottle = new Bottle(o_id, capacity, surplus);
+                String bottleTask = (String) JSON.toJSON(bottle);
+
+                String type = attributess.getType();
+                Water water = new Water(o_id, type, capacity*surplus);
+                String waterTask = (String) JSON.toJSON(water);
+
+                tops.add(bottleTask);
+                tops.add(waterTask);
+
+                surplus=0;
 
             }
 
-        }else {
-            Order order = JSON.parseObject(json, Order.class);
-            int o_id = order.getO_id();
-            String s_vals = order.getS_vals();
-            CommodityAttributes attributes = JSON.parseObject(s_vals, CommodityAttributes.class);
-
-            short capacity = attributes.getCapacity();
-            int num = attributes.getNum();
-            Bottle bottle = new Bottle(o_id, capacity, num);
-            String bottleTask = (String) JSON.toJSON(bottle);
-
-            String type = attributes.getType();
-            long total = attributes.getTotal();
-            Water water = new Water(o_id, type, total);
-            String waterTask = (String) JSON.toJSON(water);
         }
 
-        String[] task={bottleTask,waterTask};
+        String[] task = new String[tops.size()];
+
+        for (int i=0;i<=tops.size();i++){
+
+            task[i]=tops.get(i);
+
+        }
 
         return task;
 
@@ -74,3 +113,4 @@ public class Implementer implements Callable {
     }
 
 }
+
